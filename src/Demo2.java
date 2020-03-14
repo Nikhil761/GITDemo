673b43b1321ee16063234cf1d3864e9aa4727bbe
nikhil <nikhilkhamkarhatti1@gmail.com>
2020-03-14 19:59:20 +0530

    x1-1 added one file

---
+++ src/Demo2.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//selenium code
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Nik\\Downloads\\chromedriver_win32 (2)\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();	
+		driver.get("https://www.google.com/");
+		System.out.println(driver.getTitle());
+		System.out.println("This is GitX change");
+		System.out.println("This is GitX change2");
+		System.out.println("This is GitX change3");
+		System.out.println("This is GitX change4");
+		System.out.println("This is GitX change5");
+		System.out.println("This is GitX change6");
+		System.out.println("This is GitX change7");
+		System.out.println("This is GitX change8");
+	}
+
+}
